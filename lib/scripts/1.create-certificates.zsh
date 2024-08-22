29993f978670df90fc97ce8cf9b0e50dccdffd75
ilDug <dognini.marco@gmail.com>
2024-08-22 18:31:18 +0200

    fixing certificate creation

--- lib/scripts/1.create-certificates.zsh
+++ lib/scripts/1.create-certificates.zsh
@@ -1,23 +1,39 @@
 #! /bin/zsh
 
-# ask if the user wants to create the certificates for DEVELOPMENT
-echo "La creazione dei certificati è necessaria solo se si usa l'ambiente di sviluppo DEVELEPMENT."
-echo "in ambiente di produzione, i certificati sono generati in automatico alla creazione del container."
-echo "Do you want to create the certificates for DEVELOPMENT? (y/n)"
+# ask if the user wants to create the certificates 
+echo "Do you want to create the certificates for PRODUCTION? (y/n)"
 read CREATE_CERTIFICATES
 
-# check if the user wants to create the certificates for DEVELOPMENT
-if [ $CREATE_CERTIFICATES = "y" ]; then
+
     # define paths
-    CNF=lib/config/openssl/openssl.cnf
-    CERTS=backend/lib/certs
-    KEYS=backend/lib/keys
+CERTS=lib/secrets/certs
+KEYS=lib/secrets/keys
+PATHS=(
+    $CERTS
+    $KEYS
+)
+DEST=backend/lib/
+CNF=lib/config/openssl/openssl.cnf
+
+
+ 
+# check if the user wants to create the certificates
+if [ $CREATE_CERTIFICATES = "y" ]; then
+    mkdir -p "${PATHS[@]}"
 
     # create directories
     mkdir -p $CERTS $KEYS
 
     # create  certificate key pair
     openssl req -x509 -nodes -days 3650 -newkey rsa:4096 -keyout $KEYS/auth.key -out $CERTS/auth.crt -config $CNF
+
+    # ask if the user want to copy the certificates to the destination directory
+    echo "Do you want to copy the certificates to the DEVELOPMENT directory? (y/n)"
+    read COPY_CERTIFICATES
+    if [ $COPY_CERTIFICATES = "y" ]; then
+        cp -R $CERTS $DEST
+        cp -R $KEYS $DEST
+    fi
 fi
 
 
