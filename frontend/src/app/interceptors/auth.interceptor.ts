548fa15dafff48b570ee0b494e3160dea8961f04
ilDug <dognini.marco@gmail.com>
2024-09-07 22:17:56 +0200

    account service

--- frontend/src/app/interceptors/auth.interceptor.ts
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,21 +1,20 @@
 import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '../services';
 
 export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn) => {
-    // // inject the service
-    // const auth = inject(AuthService);
+    // inject the service
+    const auth = inject(AuthService);
 
-    // // Get the auth token from the service.
-    // const authToken = "Bearer " + auth.jwt();
+    // Get the auth token from the service.
+    const authToken = "Bearer " + auth.jwt();
 
-    // // Clone the request and replace the original headers with
-    // // cloned headers, updated with the authorization.
-    // const authReq: HttpRequest<any> = req.clone({
-    //     headers: req.headers.append("Authorization", authToken)
-    // });
+    // Clone the request and replace the original headers with
+    // cloned headers, updated with the authorization.
+    const authReq: HttpRequest<any> = req.clone({
+        headers: req.headers.append("Authorization", authToken)
+    });
 
-    // // send cloned request with header to the next handler.
-    // return next(authReq);
-    return next(req);
+    // send cloned request with header to the next handler.
+    return next(authReq);
 };
