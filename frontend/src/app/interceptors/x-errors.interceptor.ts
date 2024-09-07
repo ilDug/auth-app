24d7542058818954638d41dcf7d3d060fb08edbf
ilDug <dognini.marco@gmail.com>
2024-09-07 21:09:37 +0200

    interceptors

---
+++ frontend/src/app/interceptors/x-errors.interceptor.ts
@@ -0,0 +1,21 @@
+import { HttpErrorResponse, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { NgxToastService } from '@ildug/ngx-toast';
+import { catchError, throwError } from 'rxjs';
+
+export const xErrorsInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn) => {
+    const toast = inject(NgxToastService);
+
+    return next(req)
+        .pipe(
+            catchError((error: HttpErrorResponse) => {
+
+                const xError = error.headers.has("X-Error")
+                    ? error.headers.get("X-Error")
+                    : error.error.error || error.error.message || error.message || error.statusText
+
+                toast.error(xError);
+                return throwError(() => xError);
+            })
+        )
+};
