0df9b28c207b8682493235f044c94386185b4df9
ilDug <dognini.marco@gmail.com>
2024-09-07 21:09:25 +0200

    main configuration

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,33 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { ApplicationConfig, LOCALE_ID, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { logInterceptor } from './interceptors/log.interceptor';
+import { xErrorsInterceptor } from './interceptors/x-errors.interceptor';
+import { authInterceptor } from './interceptors/auth.interceptor';
+import { AUTH_SERVER, AUTH_TOKEN } from './injection-tokens';
+import { APP_BASE_HREF, PlatformLocation } from '@angular/common';
+
+/** Locale configuration ***************************/
+import { registerLocaleData } from '@angular/common';
+import localeIt from '@angular/common/locales/it';
+registerLocaleData(localeIt);
+/************************************************* */
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+    providers: [
+        provideExperimentalZonelessChangeDetection(),
+        provideRouter(routes, withComponentInputBinding()),
+        provideHttpClient(withInterceptors([
+            logInterceptor,
+            xErrorsInterceptor,
+            authInterceptor
+        ])),
+        provideAnimationsAsync(),
+        { provide: AUTH_TOKEN, useValue: 'dagjwt' },
+        { provide: AUTH_SERVER, useValue: 'http://auth.dag.lan/api/v1' },
+        { provide: APP_BASE_HREF, useFactory: (pl: PlatformLocation) => pl.getBaseHrefFromDOM(), deps: [PlatformLocation] },
+        { provide: LOCALE_ID, useValue: 'it-IT' }
+    ]
 };
