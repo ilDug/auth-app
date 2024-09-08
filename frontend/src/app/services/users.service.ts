ab37ec336990289844e69715897266e924c1e631
ilDug <dognini.marco@gmail.com>
2024-09-08 09:12:19 +0200

    init users service

--- frontend/src/app/services/users.service.ts
+++ frontend/src/app/services/users.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { AUTH_SERVER } from '../injection-tokens';
+import { catchError, finalize, from, lastValueFrom, map, of, switchMap, tap } from 'rxjs';
+import { User } from '../classes/user';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+    constructor() {
+        this.load();
+    }
+
+    #http = inject(HttpClient);
+
+    private serverUrl = inject(AUTH_SERVER);
+
+    collection = signal<User[]>([]);
+
+    PENDING = signal<boolean>(false);
+
+    request$ = of(true)
+        .pipe(
+            tap(_ => this.PENDING.set(_)),
+            switchMap(() => this.#http.get(`${this.serverUrl}/users`)),
+            catchError(err => of([])),
+            map((users: any[]) => users.map(u => new User(u))),
+            tap(users => this.collection.set(users)),
+            finalize(() => this.PENDING.set(false))
+        )
+
+    public load = (): Promise<User[]> => lastValueFrom(this.request$);
+
+
 }
