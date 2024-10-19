ee3f0de2085cd2c31c5696a59164cc0bac22ed79
ilDug <dognini.marco@gmail.com>
2024-10-19 20:01:07 +0200

    Add activate component and route (NON TERMINATO)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './account/login/login.component';
 import { AccountComponent } from './account/account.component';
 import { RegisterComponent } from './account/register/register.component';
+import { ActivateComponent } from './account/activate/activate.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'account', pathMatch: 'full' },
@@ -11,7 +12,8 @@ export const routes: Routes = [
         children: [
             { path: '', redirectTo: 'login', pathMatch: 'full' },
             { path: 'login', component: LoginComponent, title: 'Login' },
-            { path: 'register', component: RegisterComponent, title: 'Register' }
+            { path: 'register', component: RegisterComponent, title: 'Register' },
+            { path: 'activate/:key', component: ActivateComponent, title: 'Activate' },
         ]
     },
 ];
