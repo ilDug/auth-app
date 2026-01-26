36813d0061f3af0fb111b0472d22f32582084843
ilDug <dognini.marco@gmail.com>
2026-01-26 15:53:52 +0100

    Initialize FastAPI application and add logging setup; create .dockerignore file

--- api/main.py
+++ api/main.py
@@ -1,6 +1,77 @@
-def main():
-    print("Hello from api!")
+import fastapi
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import PlainTextResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.exceptions import RequestValidationError
+from datetime import datetime
+
+from core.config import CORS
+from core.middlewares import validation_exception_handler, http_rewrite_header_handler
+import logging
+
+# LOGGING SETUP
+# ###########################################################
+from icecream import ic
+
+ic.configureOutput(includeContext=True, prefix="DAG LOG | ")
+
+log_formatter = logging.Formatter(
+    fmt="%(asctime)s - DAG - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(log_formatter)
+# file_handler = logging.FileHandler("auth-app.log")
+# file_handler.setFormatter(log_formatter)
+logger = logging.getLogger("auth-app")
+logger.setLevel(logging.WARNING)
+logger.addHandler(console_handler)
+# logger.addHandler(file_handler)
+# ###########################################################
 
 
-if __name__ == "__main__":
-    main()
+ic("Starting auth-app... ")
+
+
+app = FastAPI(
+    title="auth-app",
+    version="1.3.0",
+    description="Authentication and Authorization server backend",
+)
+
+
+# MIDDLEWARE
+app.add_middleware(CORSMiddleware, **CORS)
+
+
+#  EXCEPTION HANDLERS
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
+app.add_exception_handler(HTTPException, http_rewrite_header_handler)
+
+# ROUTERS
+
+
+#  STATIC FILES
+# app.mount("/assets", StaticFiles(directory=ASSETS_PATH), name="static_media")
+
+
+# MAIN ROUTE
+@app.get("/", response_class=PlainTextResponse)
+async def root():
+    return f"""AUTH SERVER VERSION {app.version},
+RUNNING ON FASTAPI {fastapi.__version__}.
+Server time: {datetime.now()} (isoformat: {datetime.now().isoformat()})
+"""
+
+
+# HEALTH CHECK for load balancer
+@app.get("/health")
+async def check():
+    return True
+
+
+# @app.get("/upgrade")
+# async def upgrade():
+#     # aggiorna gliaccount aggiungendo le chiavi mancanti
+#     updated_accounts = add_keys_to_all_accounts()
+#     return {"updated_accounts": updated_accounts}
